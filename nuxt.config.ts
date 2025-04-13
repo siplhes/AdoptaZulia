6fed7b82af132bd8edda9170310af28712e066ca
Joseph Hurtado <91851611+siplhes@users.noreply.github.com>
2025-04-12 22:12:00 -0400

    feat: disable server-side rendering in nuxt configuration

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,7 +4,7 @@ import { defineNuxtConfig } from 'nuxt/config'
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: false },
-  ssr: true,
+  ssr: false,
 
   modules: [
     '@nuxt/eslint',
